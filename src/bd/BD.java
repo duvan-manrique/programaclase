f5438268b97f0834b262b112a50aee9bd22f9106
Unknown <lokus.09@gmail.com>
2018-04-18 20:20:06 -0500

    prima

---
+++ src/bd/BD.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+/**
+ *
+ * @author home
+ */
+public class BD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        acceso a = new acceso();
+        a.setVisible(true);
+    }
+    
+}
